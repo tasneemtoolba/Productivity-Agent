5bbd13eb5e3c55f6d9f16c5599db179268105934
tasneemtoolba <tasneemsherif23@gmail.com>
2025-02-08 00:29:16 +0200

    add usdc rpc urls

--- smart-contract/script/Vault.s.sol
+++ smart-contract/script/Vault.s.sol
@@ -14,7 +14,9 @@ contract VaultScript is Script {
 
         //_addressesProvider testnet = 0x012bAC54348C0E635dCAc9D5FB99f06F24136C9A
         //_addressesProvider mainnet = 0x2f39d218133AFaB8F2B819B1066c7E434Ad94E9e
-        vault = new Vault(0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238,0x012bAC54348C0E635dCAc9D5FB99f06F24136C9A);
+        // usdc testnet = 0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238
+        // usdc mainnet = 0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48
+        vault = new Vault(0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238, 0x012bAC54348C0E635dCAc9D5FB99f06F24136C9A);
 
         vm.stopBroadcast();
     }
