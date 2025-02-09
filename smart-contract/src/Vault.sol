6b0a85bf78822d85b8919737aea8304299e35b31
Baptiste Florentin <baptiste.florentin.pro@gmail.com>
2025-02-09 16:28:38 +0100

    ready to deploy

--- smart-contract/src/Vault.sol
+++ smart-contract/src/Vault.sol
@@ -5,33 +5,47 @@ pragma solidity <=0.8.28;
 import {IPool} from "@aave/aave-v3-core/contracts/interfaces/IPool.sol";
 import {IPoolAddressesProvider} from "@aave/aave-v3-core/contracts/interfaces/IPoolAddressesProvider.sol";
 import {IAToken} from "@aave/aave-v3-core/contracts/interfaces/IAToken.sol";
-import {IERC20} from "@aave/aave-v3-core/contracts/dependencies/openzeppelin/contracts/IERC20.sol";
-import {ERC20} from "@aave/aave-v3-core/contracts/dependencies/openzeppelin/contracts/ERC20.sol";
 import "forge-std/src/console.sol";
+import {Ownable} from "openzeppelin-contracts/contracts/access/Ownable.sol";
+import {ERC20} from "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 
-contract Vault is ERC20 {
+// TODO use checkpoint math to make sure no one can "flash steal" the rewards
+contract Vault is ERC20, Ownable {
     IPoolAddressesProvider public immutable ADDRESSES_PROVIDER;
     IPool public immutable POOL;
-    address payable owner;
 
-    IERC20 public immutable usdcToken =
-        IERC20(0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913);
-    IERC20 public immutable aBasUsdc =
-        IERC20(0x4e65fE4DbA92790696d040ac24Aa414708F5c0AB);
+    IERC20 public immutable usdcToken;
+    IERC20 public immutable aUsdcToken;
 
-    mapping(address => uint256) public userRewards;
+    uint256 totalClaimed;
+    mapping(address => uint256) public claimedRewards; // user => claimed rewards
+    mapping(address => uint256) public slashed; // user => slashed amount out of 1e18
 
-    constructor(address _addressProvider) ERC20("VaultUSDC", "vUSDC") {
-        owner = payable(msg.sender);
+    address productivityGodAgent;
+
+    uint256 public daoRewards;
+
+    event SlashUser(address indexed _user, uint256 _amount);
+
+    constructor(
+        address _addressProvider,
+        address _usdcToken,
+        address _aUsdcToken
+    ) ERC20("Productivity God USDC", "pgUSDC") Ownable(msg.sender) {
         ADDRESSES_PROVIDER = IPoolAddressesProvider(_addressProvider);
         POOL = IPool(ADDRESSES_PROVIDER.getPool());
+        IERC20(_usdcToken).approve(address(POOL), type(uint256).max);
+
+        usdcToken = IERC20(_usdcToken);
+        aUsdcToken = IERC20(_aUsdcToken);
     }
 
     function deposit(uint256 _amount) external {
         require(_amount > 0, "Amount must be greater than 0");
-        _mint(msg.sender, _amount);
         usdcToken.transferFrom(msg.sender, address(this), _amount);
         _supplyLiquidity(_amount);
+        _mint(msg.sender, _amount);
     }
 
     function _supplyLiquidity(uint256 _amount) internal {
@@ -43,7 +57,6 @@ contract Vault is ERC20 {
         address onBehalfOf = address(this);
         uint16 referralCode = 0;
         usdcToken.approve(address(POOL), _amount);
-        console.log(usdcToken.balanceOf(address(this)));
         POOL.supply(address(usdcToken), _amount, onBehalfOf, referralCode);
     }
 
@@ -73,42 +86,55 @@ contract Vault is ERC20 {
     // and x is the percentage of reward that was slashed by the users’ lack of productivity.
     // _slashed is out of 10e18 ether. So 10e18 would be 100%, 10e17 would be 10%, etc
 
-    function checkDAOReward() external view returns (uint256) {
-        return aBasUsdc.balanceOf(address(this));
-    }
-
-    function checkUserReward(
-        address _user,
-        uint256 _slashed
-    ) external view returns (uint256) {
-        return calculateUserReward(_user, _slashed);
-    }
-
-    function calculateUserReward(
-        address _user,
-        uint256 _slashed
-    ) internal view returns (uint256) {
+    function calculateRewards(
+        address _user
+    ) public view returns (uint256 _userRewards, uint256 _daoRewards) {
         if (totalSupply() == 0) {
-            return 0;
+            _userRewards = 0;
+            _daoRewards = 0;
         }
-        require(_slashed <= 1e18, "invalid _slashed value");
+
+        uint256 baseReward = ((aUsdcToken.balanceOf(address(this)) -
+            totalSupply() +
+            totalClaimed) * balanceOf(_user)) /
+            totalSupply() -
+            claimedRewards[_user];
+
         // Calculate the final reward after slashing
-        return
-            ((1e18 - _slashed) *
-                (aBasUsdc.balanceOf(address(this)) - totalSupply()) *
-                balanceOf(_user)) / (1e18 * totalSupply());
+        _userRewards = ((1e18 - slashed[_user]) * baseReward) / 1e18;
+        _daoRewards = (slashed[_user] * baseReward) / 1e18;
+    }
+
+    function setProductivityGodAgent(address _agent) external onlyOwner {
+        productivityGodAgent = _agent;
+    }
+
+    function slashUser(address _user, uint256 _amount) external {
+        require(msg.sender == productivityGodAgent, "Not agent");
+        slashed[_user] = slashed[_user] + _amount > 1 ether
+            ? slashed[_user] + _amount
+            : 1 ether;
+
+        emit SlashUser(_user, slashed[_user]);
     }
 
     function claimDAOReward() external {
-        withdrawlLiquidity(aBasUsdc.balanceOf(address(this)));
+        withdrawlLiquidity(daoRewards);
+        daoRewards = 0;
     }
 
-    function claimUserRewards(address _user, uint256 _slashed) external {
+    function claimUserRewards(address _user) external {
         // here you take the msg.sender, you check its amount of protocol tokens, send the share of rewards minus what was slashed, send the slashed amount to the DAO
         // require(balanceOf(msg.sender)>=_amount,"withdrawing more than the available");
-        uint256 calculatedRewards = calculateUserReward(_user, _slashed);
-        withdrawlLiquidity(calculatedRewards);
-        usdcToken.transfer(msg.sender, calculatedRewards);
+        (uint256 _userRewards, uint256 _daoRewards) = calculateRewards(_user);
+
+        totalClaimed += _userRewards + _daoRewards;
+        claimedRewards[_user] += _userRewards;
+        daoRewards += _daoRewards;
+        slashed[_user] = 0;
+
+        withdrawlLiquidity(_userRewards);
+        usdcToken.transfer(_user, _userRewards);
     }
 
     function getUserAccountData(
@@ -128,25 +154,14 @@ contract Vault is ERC20 {
         return POOL.getUserAccountData(_userAddress);
     }
 
-    function approve(uint256 _amount) external returns (bool) {
-        return usdcToken.approve(address(POOL), _amount);
-    }
-
-    function allowance() external view returns (uint256) {
-        return usdcToken.allowance(address(this), address(POOL));
-    }
-
-    function getBalance() external view returns (uint256) {
-        return usdcToken.balanceOf(address(this));
-    }
-
-    modifier onlyOwner() {
-        require(
-            msg.sender == owner,
-            "Only the contract owner can call this function"
-        );
-        _;
-    }
-
     receive() external payable {}
+
+    function execute(
+        address to,
+        uint256 value,
+        bytes calldata data
+    ) external payable onlyOwner returns (bool, bytes memory) {
+        (bool success, bytes memory result) = to.call{value: value}(data);
+        return (success, result);
+    }
 }
